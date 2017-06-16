25dad2bf7ea8e3c1ed9a861399c54f72d473adad
jianglianghao <jianglianghao88@126.com>
2017-06-16 18:22:05 +0800

    上传多张图片

--- xinbaba/parent191/babasport-console/src/main/java/cn/itcast/core/controller/UploadController.java
+++ xinbaba/parent191/babasport-console/src/main/java/cn/itcast/core/controller/UploadController.java
@@ -1,6 +1,8 @@
 package cn.itcast.core.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import cn.itcast.common.web.Contants;
@@ -35,5 +38,24 @@ public class UploadController {
 		response.setContentType("application/json;charset=UTF-8");
 		
 		response.getWriter().write(jo.toString());
+	}
+	@RequestMapping("upload/uploadPics.do")
+	@ResponseBody
+	public List<String> uploadPics(@RequestParam(required=false) MultipartFile[] pics , HttpServletResponse response) throws IOException{
+		
+		List<String> list = new ArrayList<>();
+		
+		if(pics != null){
+			for (MultipartFile pic : pics) {
+				
+				String path = uploadService.uploadPic(pic.getBytes(),pic.getOriginalFilename());
+				String url = Contants.IMG_URL + path;
+				list.add(url);
+			}
+		}
+		
+		
+		
+	return list;
 	}
 }
